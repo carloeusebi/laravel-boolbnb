c38773d8a1b32795781f6e43a0bc68e92f2edc6d
MichaelMantovani <michmantovani@gmail.com>
2023-09-22 15:48:02 +0200

    create cards table migration

---
+++ database/migrations/2023_09_22_133110_create_cards_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->char('number', 16)->nullable();
+            $table->date('expire_date')->nullable();
+            $table->char('security_code', 3)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cards');
+    }
+};
