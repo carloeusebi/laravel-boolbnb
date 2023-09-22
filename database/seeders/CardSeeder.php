cc6377e710ef93bdeec7305cccb7d50b3ce208de
MichaelMantovani <michmantovani@gmail.com>
2023-09-22 16:34:39 +0200

    create cards Seeder

--- database/seeders/CardSeeder.php
+++ database/seeders/CardSeeder.php
@@ -17,9 +17,10 @@ class CardSeeder extends Seeder
     {
         for ($i = 0; $i < 10; $i++) {
             $card = new Card();
+            $card->user_id = 1;
             $card->first_name = $faker->firstName();
             $card->last_name = $faker->lastName();
-            $card->number = $faker->randomNumber(16, true);
+            $card->number = $faker->numerify('################');
             $card->expire_date = $faker->dateTime();
             $card->security_code = $faker->randomNumber(3, true);
             $card->save();
