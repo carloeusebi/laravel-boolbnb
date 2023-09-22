90687918b0a916cebdc1d620d1c7d198b93c5a8c
MichaelMantovani <michmantovani@gmail.com>
2023-09-22 17:15:40 +0200

    create seeder for sponsorship

---
+++ database/seeders/SponsorshipSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Sponsorship;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SponsorshipSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $sponsorships = config('sponsorships');
+        foreach ($sponsorships as $sponsorship) {
+            $new_sponsorship = new Sponsorship();
+            $new_sponsorship->price = $sponsorship['price'];
+            $new_sponsorship->duration = $sponsorship['duration'];
+            $new_sponsorship->save();
+        }
+    }
+}
