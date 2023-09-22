d422d2831ce925e82ab04970782cbe85ddb18d47
MichaelMantovani <michmantovani@gmail.com>
2023-09-22 16:10:10 +0200

    create CardSeeder with faker

---
+++ database/seeders/CardSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Card;
+use Faker\Generator;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class CardSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Generator $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $card = new Card();
+            $card->first_name = $faker->firstName();
+            $card->last_name = $faker->lastName();
+            $card->number = $faker->randomNumber(16, true);
+            $card->expire_date = $faker->dateTime();
+            $card->security_code = $faker->randomNumber(3, true);
+            $card->save();
+        }
+    }
+}
