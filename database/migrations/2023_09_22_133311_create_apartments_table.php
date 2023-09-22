08a71439f63561cd2c3a85d2c727ae1424156b18
Flopiras <Flopiras@yahoo.it>
2023-09-22 16:35:41 +0200

    temporary comment foreignId

--- database/migrations/2023_09_22_133311_create_apartments_table.php
+++ database/migrations/2023_09_22_133311_create_apartments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('apartments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            // $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name', 60);
             $table->string('slug')->unique;
             $table->tinyInteger('rooms')->nullable;
