eadb9a0c777d7bdb17e4c176d5e77df7e972f5b6
Flopiras <Flopiras@yahoo.it>
2023-09-22 15:43:05 +0200

    create migrations' table

---
+++ database/migrations/2023_09_22_133311_create_apartments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 60);
+            $table->string('slug')->unique;
+            $table->tinyInteger('rooms')->nullable;
+            $table->tinyInteger('bedrooms')->nullable;
+            $table->tinyInteger('bathrooms')->nullable;
+            $table->tinyInteger('square_meters')->nullable;
+            $table->string('image')->nullable;
+            $table->text('description')->nullable;
+            $table->boolean('is_published')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
