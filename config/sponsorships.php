d9c3efb891c310ef32968155fb4a332bf3bad8e7
MichaelMantovani <michmantovani@gmail.com>
2023-09-22 17:15:09 +0200

    add sponsorships array in config

---
+++ config/sponsorships.php
@@ -0,0 +1,15 @@
+<?php
+return $sponsorships = [
+  [
+    'price' => 2, 99,
+    'duration' => 24
+  ],
+  [
+    'price' => 5, 99,
+    'duration' => 72
+  ],
+  [
+    'price' => 9, 99,
+    'duration' => 144
+  ],
+];
