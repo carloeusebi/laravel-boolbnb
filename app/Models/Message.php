f41fdfdba8e6d5405da08dc976bc2b9bf18a1cf8
Flopiras <Flopiras@yahoo.it>
2023-09-22 17:04:33 +0200

    create model message

---
+++ app/Models/Message.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Message extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['title', 'content', 'email'];
+
+    public function apartment()
+    {
+        return $this->belongsTo(Apartment::class);
+    }
+}
