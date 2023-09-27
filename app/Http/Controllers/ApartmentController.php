0214b79e04ef82f90a4f6a18584d059231b6b6e6
carloeusebi <carloeusebi@gmail.com>
2023-09-27 11:14:32 +0200

    Improved thumbnail upload

--- app/Http/Controllers/ApartmentController.php
+++ app/Http/Controllers/ApartmentController.php
@@ -33,23 +33,11 @@ class ApartmentController extends Controller
     {
         $data = $request->all();
 
+        $thumbnail = $request->file('thumbnail');
+        $data['thumbnail'] = $this->saveImage($thumbnail);
+
         //slug
         $data['slug'] = Str::slug($request->name);
-
-        // IMAGE UPLOAD
-        if (array_key_exists('thumbnail', $data)) {
-            $data['thumbnail'] = $this->saveImage($data['thumbnail']);
-        }
-
-        // COORDINATES
-        try {
-            $coordinates = $this->getCoordinates($request->address);
-        } catch (Exception $e) {
-            $code = $e->getCode() === 422 ? 422 : 500;
-            return response()->json(['errors' => $e->getMessage()], $code);
-        }
-        $data['lat'] = $coordinates['lat'];
-        $data['lon'] = $coordinates['lon'];
 
         $apartment = Apartment::create($data);
 
@@ -77,24 +65,15 @@ class ApartmentController extends Controller
         $data = $request->all();
 
         // IMAGE UPLOAD
-        if (array_key_exists('thumbnail', $data)) {
-            // if there was a previous thumbnail, delete the old thumbnail
-            if ($apartment->thumbnail)
+        $thumbnail = $request->file('thumbnail');
+        if ($thumbnail){
+
+            // if there was an old thumbnail delete it
+            if ($apartment->thumbnail){
                 Storage::delete($apartment->thumbnail);
-
-            $data['thumbnail'] = $this->saveImage($data['thumbnail']);
-        }
-
-        // COORDINATES
-        if ($apartment->address !== $request->address) {
-            try {
-                $coordinates = $this->getCoordinates($request->address);
-            } catch (Exception $e) {
-                $code = $e->getCode() === 422 ? 422 : 500;
-                return response()->json(['errors' => $e->getMessage()], $code);
             }
-            $data['lat'] = $coordinates['lat'];
-            $data['lon'] = $coordinates['lon'];
+
+            $data['thumbnail'] = $this->saveImage($thumbnail);
         }
 
         $apartment->update($data);
@@ -127,31 +106,5 @@ class ApartmentController extends Controller
     private function saveImage($image)
     {
         return Storage::put('images', $image);
-    }
-
-
-    /**
-     * Makes an api call to the 
-     * 
-     * @param string $address The Address string.
-     * @return array An array containing the coordinate's keys `lan` and `lon`
-     */
-    private function getCoordinates(string $address)
-    {
-        $api_key = env('TOM_TOM_KEY');
-
-        $response = Http::withUrlParameters([
-            'endpoint' => 'https://api.tomtom.com/search/2/geocode',
-            'address' => "$address.json",
-            'key' => $api_key,
-        ])->get('{+endpoint}/{address}?key={key}');
-
-        $response->throw();
-
-        if ($response['summary']['totalResults'] === 0)
-            throw new \Exception('Indirizzo non valido.', 422);
-
-        $coordinates = $response['results'][0]['position'];
-        return $coordinates;
     }
 }
