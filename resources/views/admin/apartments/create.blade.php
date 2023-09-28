5e9b3919d32a038d8fed66bfc58d92a1c128455a
Flopiras <Flopiras@yahoo.it>
2023-09-28 17:18:16 +0200

    add title' section

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -1,8 +1,10 @@
-@extends('layout.app')
+@extends('layouts.app')
+
+@section('title', 'Crea nuovo appartamento')
 
 @section('content')
   {{-- back button --}}
-  <a href="{{ route('admin.apartments.index') }}" class="btn btn-secondary">Torna alla lista</a>
+  <a href="{{ route('admin.apartments.index') }}" class="btn btn-secondary my-4">Torna alla lista</a>
 
   {{-- form --}}
   @include('includes.form')
