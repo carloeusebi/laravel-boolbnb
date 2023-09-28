f79b0c72855993690276b5183e561c6ed3c26a3a
MichaelMantovani <michmantovani@gmail.com>
2023-09-28 18:15:03 +0200

    Edit responsive layout

--- resources/views/admin/apartments/trash.blade.php
+++ resources/views/admin/apartments/trash.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app');
+@extends('layouts.app')
 
 @section('title', 'Cestino appartamenti')
 
@@ -18,18 +18,20 @@
           <td>{{ $apartment->name }}</td>
           <td>{{ $apartment->getDescription() }}</td>
           <td>{{ $apartment->address }}</td>
-          <td class="d-flex gap-2">
-            <a class="btn btn-primary" href="{{ route('admin.apartments.show', $apartment) }}">Vedi</a>
-            <form action="{{ route('admin.apartments.restore', $apartment->id) }}" method="POST" class="mb-0">
-              @method('put')
-              @csrf
-              <button type="submit" class="btn btn-success">Rispristina</button>
-            </form>
-            <form action="{{ route('admin.apartments.drop', $apartment->id) }}" method="Post" class="mb-0">
-              @method('delete')
-              @csrf
-              <button type="submit" class="btn btn-danger">Elimina</button>
-            </form>
+          <td>
+            <div class="d-flex gap-2 flex-sm-column flex-lg-row">
+              <a class="btn btn-primary" href="{{ route('admin.apartments.show', $apartment) }}">Vedi</a>
+              <form action="{{ route('admin.apartments.restore', $apartment->id) }}" method="POST" class="mb-0">
+                @method('put')
+                @csrf
+                <button type="submit" class="btn btn-success">Rispristina</button>
+              </form>
+              <form action="{{ route('admin.apartments.drop', $apartment->id) }}" method="Post" class="mb-0">
+                @method('delete')
+                @csrf
+                <button type="submit" class="btn btn-danger">Elimina</button>
+              </form>
+            </div>
           </td>
         </tr>
       @endforeach
