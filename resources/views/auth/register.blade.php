a4af0aedfea90b8d405affa5a40d8289bbfa1e8d
MichaelMantovani <michmantovani@gmail.com>
2023-10-02 11:43:01 +0200

    add mark for required field

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -42,7 +42,7 @@
 
                 {{-- Email --}}
                 <div class="col-md-6">
-                  <label for="email" class="form-label text-md-right">Email</label>
+                  <label for="email" class="form-label text-md-right">Email *</label>
                   <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
                     name="email" value="{{ old('email') }}" required autocomplete="email">
                   @error('email')
@@ -69,7 +69,7 @@
 
                 {{-- Password --}}
                 <div class="col-md-6">
-                  <label for="password" class="form-label text-md-right">{{ __('Password') }}</label>
+                  <label for="password" class="form-label text-md-right">{{ __('Password') }} *</label>
                   <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
                     name="password" required autocomplete="new-password">
                   @error('password')
@@ -81,7 +81,7 @@
 
                 {{-- Password confirm --}}
                 <div class="col-md-6">
-                  <label for="password-confirm" class="form-label text-md-right">{{ __('Confirm Password') }}</label>
+                  <label for="password-confirm" class="form-label text-md-right">{{ __('Confirm Password') }} *</label>
                   <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required
                     autocomplete="new-password">
                 </div>
