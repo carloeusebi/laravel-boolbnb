0a76a6cb37eac5b4c7b73840aba149c56c7bf43a
MichaelMantovani <michmantovani@gmail.com>
2023-10-06 17:41:27 +0200

    create content messagge getter

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Message extends Model
 {
@@ -17,5 +18,10 @@ class Message extends Model
     public function apartment()
     {
         return $this->belongsTo(Apartment::class);
+    }
+
+    public function getMessage()
+    {
+        return Str::limit($this->content, 40);
     }
 }
