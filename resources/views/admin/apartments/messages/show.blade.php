db310f6b3209c7163c5ca3e4d51da75b21aefebc
MichaelMantovani <michmantovani@gmail.com>
2023-10-06 17:42:16 +0200

    create message show

---
+++ resources/views/admin/apartments/messages/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="d-flex justify-content-center align-items-center">
+        <div class="card mt-5 w-75">
+            <div class="card-header">
+                {{ $message->apartment->name }} - {{ $message->apartment->address }}
+            </div>
+            <div class="card-body">
+                <p class="card-title fs-5">{{ $message->name }} - {{ $message->email }}</p>
+                <p class="card-text mt-3">{{ $message->content }}</p>
+                <a href="#" class="btn btn-primary btn-sm">Rispondi</a>
+                <a class="btn btn-sm btn-secondary btn-sm" href="{{ route('admin.apartments.messages') }}"><i
+                        class="me-2 fa-solid fa-circle-left"></i>Torna agli appartamenti</a>
+            </div>
+        </div>
+    </div>
+@endsection
