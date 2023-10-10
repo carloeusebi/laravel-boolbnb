0e2d91b3472f853d17302e5000df03298b38ad7b
MichaelMantovani <michmantovani@gmail.com>
2023-10-10 17:50:25 +0200

    edit mail reply form

--- resources/views/admin/apartments/messages/mails/create.blade.php
+++ resources/views/admin/apartments/messages/mails/create.blade.php
@@ -2,21 +2,30 @@
 
 
 @section('content')
-    <form class="row g-3 mt-5" method="POST" action="{{ route('admin.apartments.messages.mails.reply') }}">
+    <form class="row g-3 mt-5" method="POST" action="{{ route('admin.apartments.messages.mails.reply', $message) }}">
+        @csrf
         <p class="mb-4 text-end" novalidate>
             * I campi sono obbligatori
         </p>
         <div class="col-md-6">
-            <label for="admin-name" class="form-label">Nome*</label>
-            <input type="text" class="form-control" id="admin-name" name="admin-name">
+            <label for="subject" class="form-label">Oggetto*</label>
+            <input type="text" class="form-control" id="subject" name="subject">
         </div>
         <div class="col-md-6">
-            <label for="admin-email" class="form-label">Email*</label>
-            <input type="email" class="form-control" id="admin-email" name="admin-email">
+            <label for="admin_email" class="form-label">Email*</label>
+            <input type="email" class="form-control" id="admin_email" name="admin_email"
+                value="{{ Auth::user()->email }}">
         </div>
+        <div class="col-md-6 d-none">
+            <input type="email" class="form-control" id="user_email" name="user_email" value="{{ $message->email }}">
+            <input type="text" class="form-control" id="user_message_id" name="user_message_id"
+                value="{{ $message->id }}">
+
+        </div>
+
         <div class="col-12">
-            <label for="message" class="form-label">Messaggio*</label>
-            <textarea name="message" id="message" cols="30" rows="10" class="form-control" name="message"></textarea>
+            <label for="content" class="form-label">Messaggio*</label>
+            <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
         </div>
         <div class="col-12">
             <button type="submit" class="btn btn-primary">Invia Mail</button>
