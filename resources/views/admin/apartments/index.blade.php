d6cf5184977c19dab6072d5e562acf428dde3794
carloeusebi <carloeusebi@gmail.com>
2023-10-11 17:20:56 +0200

    Improved how no apartments are displayed on index

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -13,54 +13,54 @@
         <!-- HEADER: -->
         <h1 class="text-center h4 mt-5">I miei appartamenti</h1>
         <hr>
-        <div class="d-flex justify-content-end align-items-center">
-            <a class="text-center btn btn-sm btn-success fw-bold" href="{{ route('admin.apartments.create') }}">+ Aggiungi
-                nuovo</a>
-        </div>
 
-        <!-- TABLE: -->
-        <table class="table mb-5 mt-3 table-striped ">
-            <thead>
-                <tr>
-                    <th class="d-none d-lg-table-cell" scope="col">#</th>
-                    <th scope="col">Titolo</th>
-                    <th class="d-none d-md-table-cell" scope="col">Descrizione</th>
-                    <th scope="col">Indirizzo</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($apartments as $apartment)
+        @if ($apartments->count() > 0)
+            <div class="d-flex justify-content-end align-items-center">
+                <a class="text-center btn btn-sm btn-success fw-bold" href="{{ route('admin.apartments.create') }}">+ Aggiungi
+                    nuovo</a>
+            </div>
+
+            <!-- TABLE: -->
+            <table class="table mb-5 mt-3 table-striped ">
+                <thead>
                     <tr>
-                        <th class="d-none d-lg-table-cell" scope="row">{{ $apartment->id }}</th>
-                        <td>{{ $apartment->name }}</td>
-                        <td class="d-none d-md-table-cell">{{ $apartment->getDescription() }}</td>
-                        <td>{{ $apartment->address }}</td>
+                        <th scope="col">Nome</th>
+                        <th class="d-none d-md-table-cell" scope="col">Descrizione</th>
+                        <th scope="col">Indirizzo</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($apartments as $apartment)
+                        <tr>
+                            <td>{{ $apartment->name }}</td>
+                            <td class="d-none d-md-table-cell">{{ $apartment->getDescription() }}</td>
+                            <td>{{ $apartment->address }}</td>
 
-                        <td>
-                            <div class="d-flex justify-content-end  h-100">
+                            <td>
+                                <div class="d-flex justify-content-end  h-100">
 
-                                <!--show-->
-                                <a class="text-white fw-bold text-decoration-none btn btn-sm btn-primary"
-                                    href="{{ route('admin.apartments.show', $apartment) }}">
-                                    <i class="fas fa-eye"></i>
-                                </a>
+                                    <!--show-->
+                                    <a class="text-white fw-bold text-decoration-none btn btn-sm btn-primary"
+                                        href="{{ route('admin.apartments.show', $apartment) }}">
+                                        {{-- <i class="fas fa-eye"></i> --}}
+                                        Dettagli
+                                    </a>
 
-                                <form action="{{ route('admin.apartments.messages', $apartment) }}" method="GET">
-                                    @csrf
-                                    <input type="text" name="id" value="{{ $apartment->id }}" class="d-none">
-                                    <button class="ms-2 text-white fw-bold text-decoration-none btn btn-sm btn-primary"><i
-                                            class="fas fa-envelope"></i>
-                                    </button>
-                                </form>
+                                    <form action="{{ route('admin.apartments.messages', $apartment) }}" method="GET">
+                                        @csrf
+                                        <input type="text" name="id" value="{{ $apartment->id }}" class="d-none">
+                                        <button
+                                            class="ms-2 text-white fw-bold text-decoration-none btn btn-sm btn-primary"><i
+                                                class="fas fa-envelope"></i>
+                                        </button>
+                                    </form>
 
-                                <!--edit-->
-                                <a class="ms-2 text-white fw-bold text-decoration-none btn btn-sm btn-warning"
+                                    {{-- <a class="ms-2 text-white fw-bold text-decoration-none btn btn-sm btn-warning"
                                     href="{{ route('admin.apartments.edit', $apartment) }}">
                                     <i class="fas fa-pencil"></i>
                                 </a>
 
-                                <!--delete-->
                                 <form data-bs-toggle="modal" data-bs-target="#modal" class="delete-form"
                                     action="{{ route('admin.apartments.destroy', $apartment) }}" method="POST">
                                     @csrf
@@ -71,20 +71,19 @@
                                         </a>
                                     </button>
                                 </form>
-
-                            </div>
-                        </td>
+ --}}
+                                </div>
+                            </td>
+                    @endforeach
                     </tr>
-                @empty
-                    <tr>
-                        <td class="text-center" colspan="6">
-                            <h3>Non ci sono appartamenti :(</h3>
-                        </td>
-                    </tr>
-                @endforelse
-                </tr>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        @else
+            <h3 class="h5 text-center">Non hai caricato nessun appartamento,
+                <a href="{{ route('admin.apartments.create') }}">Clicca qui</a>
+                per caricare il tuo primo appartamento
+            </h3>
+        @endif
 
     </div>
 
