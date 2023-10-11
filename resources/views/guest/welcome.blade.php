094d823d48510c626dd36c6ea2ebc55e73865dfe
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-10-11 11:57:43 +0200

    title and favicon fix

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -1,21 +1,22 @@
 @extends('layouts.app')
+@section('title', 'Home')
 @section('content')
-    <div class="home_container">
+  <div class="home_container">
 
-        <div class="jumbotron p-5 my-4 rounded-3">
-            <div class="container py-5">
+    <div class="jumbotron p-5 my-4 rounded-3">
+      <div class="container py-5">
 
-                <h1 class="display-5 fw-bold">
-                    Benveuto su BoolBnB
-                </h1>
+        <h1 class="display-5 fw-bold">
+          Benveuto su BoolBnB
+        </h1>
 
-                <p class="col-md-8 fs-4">Su questo portale potrai caricare nuovi appartamenti, modificare quelli già
-                    esistenti e
-                    avere accesso ai messaggi scritti dai visitatori dei tuoi annunci</p>
-                @if (!Auth::user())
-                    <a href="{{ url('/login') }}" class="btn btn-primary btn-lg" type="button">Accedi</a>
-                @endif
-            </div>
-        </div>
+        <p class="col-md-8 fs-4">Su questo portale potrai caricare nuovi appartamenti, modificare quelli già
+          esistenti e
+          avere accesso ai messaggi scritti dai visitatori dei tuoi annunci</p>
+        @if (!Auth::user())
+          <a href="{{ url('/login') }}" class="btn btn-primary btn-lg" type="button">Accedi</a>
+        @endif
+      </div>
     </div>
+  </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,119 +2,117 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
+  <title>Boolbnb | @yield('title')</title>
+  <link rel="icon" href="{{ asset('images/logo.png') }}" type="ico">
+  {{-- Braintree for payments --}}
+  <script src="https://js.braintreegateway.com/web/dropin/1.40.2/js/dropin.min.js"></script>
 
-    {{-- Braintree for payments --}}
-    <script src="https://js.braintreegateway.com/web/dropin/1.40.2/js/dropin.min.js"></script>
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    {{-- FontAwesome --}}
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
-        integrity='sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=='
+  {{-- FontAwesome --}}
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+    integrity='sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=='
         crossorigin='anonymous' />
 
-    <!-- Usando Vite -->
-    @vite(['resources/js/app.js'])
+  <!-- Usando Vite -->
+  @vite(['resources/js/app.js'])
 </head>
 
 <body>
-    <div id="app">
+  <div id="app">
 
-        {{-- navbar --}}
-        <nav class="navbar navbar-expand-md navbar-light shadow-sm">
-            <div class="container">
-                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-                    <div class="logo_laravel">
-                    </div>
-                    {{-- config('app.name', 'Laravel') --}}
+    {{-- navbar --}}
+    <nav class="navbar navbar-expand-md navbar-light shadow-sm">
+      <div class="container">
+        <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+          <div class="logo_laravel">
+          </div>
+          {{-- config('app.name', 'Laravel') --}}
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <!-- Left Side Of Navbar -->
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <a class="@if (Route::is('home')) active @endif nav-link" href="{{ route('home') }}">Home</a>
+            </li>
+            @auth
+              <li class="nav-item">
+                <a class="@if (Route::is('admin.apartments.index')) active @endif nav-link"
+                  href="{{ route('admin.apartments.index') }}">I miei appartamenti</a>
+              </li>
+            @endauth
+          </ul>
+
+          @yield('search-bar')
+
+          <!-- Right Side Of Navbar -->
+          <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @guest
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+              </li>
+              @if (Route::has('register'))
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('register') }}">Crea un nuovo account</a>
+                </li>
+              @endif
+            @else
+              <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                  data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                  {{ Auth::user()->first_name }}
                 </a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="@if (Route::is('home')) active @endif nav-link"
-                                href="{{ route('home') }}">Home</a>
-                        </li>
-                        @auth
-                            <li class="nav-item">
-                                <a class="@if (Route::is('admin.apartments.index')) active @endif nav-link"
-                                    href="{{ route('admin.apartments.index') }}">I miei appartamenti</a>
-                            </li>
-                        @endauth
-                    </ul>
-
-                    @yield('search-bar')
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">Crea un nuovo account</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->first_name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ url('dashboard') }}">{{ __('Dashboard') }}</a>
-                                    <a class="dropdown-item" href="{{ route('admin.apartments.messages') }}">Messaggi</a>
-                                    <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="{{ url('dashboard') }}">{{ __('Dashboard') }}</a>
+                  <a class="dropdown-item" href="{{ route('admin.apartments.messages') }}">Messaggi</a>
+                  <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
+                  <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
                                                          document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                    {{ __('Logout') }}
+                  </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                  </form>
                 </div>
-            </div>
-        </nav>
+              </li>
+            @endguest
+          </ul>
+        </div>
+      </div>
+    </nav>
 
-        {{-- main --}}
-        <main class="container">
-            {{-- alert --}}
-            @include('includes.alert')
+    {{-- main --}}
+    <main class="container">
+      {{-- alert --}}
+      @include('includes.alert')
 
-            {{-- content --}}
-            @yield('content')
-        </main>
+      {{-- content --}}
+      @yield('content')
+    </main>
 
-    </div>
+  </div>
 
-    @include('includes.modal')
+  @include('includes.modal')
 
-    @yield('scripts')
+  @yield('scripts')
 </body>
 
 </html>
